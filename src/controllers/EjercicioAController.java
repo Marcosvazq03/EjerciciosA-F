41701c465e920ebea5a8ab7b159472539fd9b486
Marcos <marcosvazq033@gmail.com>
2023-10-06 14:46:54 +0200

    EDIT archivo Controller
    Trabajo que he hecho en la hora del viernes (Editar el archivo Controller)

--- src/controllers/EjercicioAController.java
+++ src/controllers/EjercicioAController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -82,23 +83,58 @@ public class EjercicioAController implements Initializable{
     @FXML
     public void aceptar(ActionEvent event) {
         try {
-        	if (txtProfesion.getText().toString().equals("")) {
-        		String errProf = "Hay que rellenar el campo profesion\n";
+        	
+        	if (txtProfesion.getText().toString().equals("") || txtNumHermanos.getText().toString().equals("") ) {
+        		String errProf = "";
+        		if (txtProfesion.getText().toString().equals("")) {
+					errProf = "Hay que rellenar el campo profesion\n";
+				}
+        		
         		String errNumHer = "";
         		if (txtNumHermanos.getText().toString().equals("")) {
-					errNumHer = "Hay que rellenar el campo Nº Hermanos";
+					errNumHer = "Hay que rellenar el campo Nº Hermanos\n";
+				}
+        		
+        		String errDepor = "";
+        		if (cbDeporte.isSelected()) {
+					if (lvDeportes.getSelectionModel().isEmpty()) {
+						errDepor = "Tienes que indicar los deportes que practicas";
+					}
 				}
         		Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("TUS DATOS");
                 alert.setHeaderText(null);
-                alert.setContentText(errProf+errNumHer);
+                alert.setContentText(errProf+errNumHer+errDepor);
                 alert.showAndWait();
 			}else {
+				String prof = txtProfesion.getText().toString()+"\n";
+				String numH = txtNumHermanos.getText().toString()+"\n";
+				String edad = cbEdad.getSelectionModel().getSelectedItem()+"\n";
+				String sexo = "";
+				if (rbHombre.isSelected()) {
+					sexo = "Hombre\n";
+				}
+				if (rbMujer.isSelected()) {
+					sexo = "Mujer\n";
+				}
+				if (rbOtro.isSelected()) {
+					sexo = "Otro\n";
+				}
+				String deport="";
+				if (cbDeporte.isSelected()) {
+					ObservableList<String> lsD = lvDeportes.getSelectionModel().getSelectedItems();
+					for (int i = 0; i < lsD.size(); i++) {
+						deport = deport + lsD.get(i)+"\n";
+					}
+					
+						
+				}
+				
 				//Ventana de informacion
 	        	Alert alert = new Alert(Alert.AlertType.INFORMATION);
 	            alert.setTitle("TUS DATOS");
 	            alert.setHeaderText(null);
-	            alert.setContentText("Profesion:");
+	            alert.setContentText("Profesion: "+prof+"Nº de hermanos: "+numH+"Edad: "+edad+"Sexo: "+sexo+"Deportes que practicas:\n"+deport);
 	            alert.showAndWait();
 			}
         	
@@ -134,7 +170,8 @@ public class EjercicioAController implements Initializable{
     	//Contenido de la lista
     	lvDeportes.setItems(FXCollections.observableArrayList("Tenis","Futbol","Baloncesto","Natacion","Ciclismo","Otro"));
     	
-    	
+    	//Para seleccionar mas de un valor en la lista
+    	lvDeportes.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
     
 
