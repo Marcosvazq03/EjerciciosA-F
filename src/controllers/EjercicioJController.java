13786472df4be566794c60a265c59a28cb798aeb
Marcos <marcosvazq033@gmail.com>
2023-10-26 22:10:06 +0200

    FIN EjercicioJ
    EjercicioJ terminado.(hacer un ToggleGroup y cambiar la visibilidad de las fotos)

--- src/controllers/EjercicioJController.java
+++ src/controllers/EjercicioJController.java
@@ -6,12 +6,19 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 
 public class EjercicioJController implements Initializable{
     
+	@FXML
+    private Button btnApagar;
+
+    @FXML
+    private Button btnEncender;
+	
 	@FXML
     private ToggleGroup colores;
 	
@@ -73,17 +80,60 @@ public class EjercicioJController implements Initializable{
     private ImageView imgMRojo;
 	
     @FXML
+    void apagar(ActionEvent event) {
+    	btnApagar.setVisible(false);
+    	btnEncender.setVisible(true);
+    	imgLuces.setVisible(true);
+    }
+
+    @FXML
+    void encender(ActionEvent event) {
+    	btnApagar.setVisible(true);
+    	btnEncender.setVisible(false);
+    	imgLuces.setVisible(false);
+    }
+    
+    @FXML
     void selectColores(ActionEvent event) {
-    	System.out.println(colores.getSelectedToggle().toString());
-    	
-    	if (colores.getSelectedToggle().toString()=="imgCAzul") {
-			imgMAzul.setVisible(true);
+    	if (colores.getSelectedToggle()!=null) {
+    		ToggleButton selectedRadioButton = (ToggleButton) colores.getSelectedToggle();
+	    	String toogleGroupValue = selectedRadioButton.getText();
+	    	imgMRojo.setVisible(false);
+	    	imgMAzul.setVisible(false);
+	    	imgMAzulOsc.setVisible(false);
+	    	imgMNegro.setVisible(false);
+	    	imgMBlanco.setVisible(false);
+	    	imgMGris.setVisible(false);
+	    	imgMGrisOsc.setVisible(false);
+	    	imgMAmarillo.setVisible(false);
+	    	if (toogleGroupValue.equals("rojo")) {
+				imgMRojo.setVisible(true);
+			}
+	    	if (toogleGroupValue.equals("azul")) {
+				imgMAzul.setVisible(true);
+			}
+	    	if (toogleGroupValue.equals("azulOsc")) {
+				imgMAzulOsc.setVisible(true);
+			}
+	    	if (toogleGroupValue.equals("negro")) {
+				imgMNegro.setVisible(true);
+			}
+	    	if (toogleGroupValue.equals("blanco")) {
+				imgMBlanco.setVisible(true);
+			}
+	    	if (toogleGroupValue.equals("gris")) {
+				imgMGris.setVisible(true);
+			}
+	    	if (toogleGroupValue.equals("grisOsc")) {
+				imgMGrisOsc.setVisible(true);
+			}
+	    	if (toogleGroupValue.equals("amarillo")) {
+				imgMAmarillo.setVisible(true);
+			}
 		}
     }
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-    	
-    	
     }
 }
