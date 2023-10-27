f4708d184276f3e2f92d3bdd43327b163b4af444
Marcos <marcosvazq033@gmail.com>
2023-10-27 15:11:21 +0200

    ADD EjercicioKController
    añadir metodo para el tiempo actual

--- src/application/EjercicioK.java
+++ src/application/EjercicioK.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -12,9 +13,10 @@ public class EjercicioK extends Application{
 	public void start(Stage primaryStage) {
         try {
             GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("/fxml/EjercicioKfxml.fxml"));
-            Scene scene = new Scene(root,700,500);
+            Scene scene = new Scene(root,400,300);
             //scene.getStylesheets().add(getClass() .getResource("/css/estilosPersona.css").toExternalForm());
             primaryStage.setTitle("RELOJ");
+            primaryStage.getIcons().add(new Image(getClass().getResource("/img/EjercicioKimg/reloj.png").toString()));
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch(Exception e) {
--- src/controllers/EjercicioKController.java
+++ src/controllers/EjercicioKController.java
@@ -2,34 +2,92 @@ package controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import dao.PersonaDao;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
-import javafx.collections.transformation.SortedList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import model.Persona;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
 
 public class EjercicioKController implements Initializable{
 
+	@FXML
+    private ImageView imgH1;
+
+    @FXML
+    private ImageView imgH2;
+
+    @FXML
+    private ImageView imgM1;
+
+    @FXML
+    private ImageView imgM2;
+
+    @FXML
+    private ImageView imgS1;
+
+    @FXML
+    private ImageView imgS2;
+    
+    private String selectImg(int num) {
+    	String img = "";
+    	switch (num) {
+	    	case 0: {
+				img = "ZERO";
+			}
+			case 1: {
+				img = "ONE";
+			}
+			case 2: {
+				img = "TWO";
+			}
+			case 3: {
+				img = "THREE";
+			}
+			case 4: {
+				img = "FOUR";
+			}
+			case 5: {
+				img = "FIVE";
+			}
+			case 6: {
+				img = "SIX";
+			}
+			case 7: {
+				img = "SEVEN";
+			}
+			case 8: {
+				img = "EIGHT";
+			}
+			case 9: {
+				img = "NINE";
+			}
+		}
+    	return img;
+    }
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		Timer timer = new Timer();
+		
+		TimerTask task = new TimerTask() {
+	        public void run() {
+	            int segundos = LocalDateTime.now().getSecond();
+	            String strSegundos = segundos<=9 ? "0"+segundos:""+segundos;
+	           
+	            int s2= Integer.parseInt(strSegundos.charAt(0) + "");
+	            int s1= Integer.parseInt(strSegundos.charAt(1) + "");
+	            
+	            imgS1.setImage(new Image(getClass().getResource("/img/EjercicioKimg/"+selectImg(s1)+".png").toString()));
+	            imgS2.setImage(new Image(getClass().getResource("/img/EjercicioKimg/"+selectImg(s2)+".png").toString()));
+				
+	        }
+	    };
+	    timer.scheduleAtFixedRate(task, 0, 1000);
 	}
 
 }
