44c4b76de9fa45ade1d0482e117d8805ebe41fa9
Marcos <marcosvazq033@gmail.com>
2023-10-27 13:53:16 +0200

    ADD EjercicioK
    Crear todos los archivos para el ejercicioK

---
+++ src/application/EjercicioK.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class EjercicioK extends Application{
+
+	@Override
+	public void start(Stage primaryStage) {
+        try {
+            GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("/fxml/EjercicioKfxml.fxml"));
+            Scene scene = new Scene(root,700,500);
+            //scene.getStylesheets().add(getClass() .getResource("/css/estilosPersona.css").toExternalForm());
+            primaryStage.setTitle("RELOJ");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+	
+}
---
+++ src/controllers/EjercicioKController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import dao.PersonaDao;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import model.Persona;
+
+public class EjercicioKController implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+	}
+
+}
